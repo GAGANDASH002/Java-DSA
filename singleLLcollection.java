7ff6244b4836ddaa0399f77e95e38a9c3d234c27
GAGANDASH002 <gagandash002@gmail.com>
2024-03-27 15:14:52 +0530

    collection frameworks on singleLL

--- singleLLcollection.java
+++ singleLLcollection.java
@@ -6,6 +6,18 @@ class LL{
         list.addFirst("Hi");
         list.addFirst("this");
         System.out.println(list);
+        list.addFirst("is");
+        list.addLast("Python");//can also be written as add() as it adds at end default
+        System.out.println(list.size());
 
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"->");//get function returns value of that node
+        }
+
+        list.removeFirst();
+        list.removeLast();
+
+        list.remove(1);//deletes value at index 1
+        System.out.println(list);
     }
 }
