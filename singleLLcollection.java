90411cef8b670f585926179f194bc64985e64042
GAGANDASH002 <gagandash002@gmail.com>
2024-03-27 15:11:12 +0530

    collection frameworks of singleLL

---
+++ singleLLcollection.java
@@ -0,0 +1,11 @@
+//single linkedlist representation using collection frameworks 
+import java.util.LinkedList;
+class LL{
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("Hi");
+        list.addFirst("this");
+        System.out.println(list);
+
+    }
+}
