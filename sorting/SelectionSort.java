1e0b67a62873affc3ee2c296a90c080a9f0b45e7
GAGANDASH002 <gagandash002@gmail.com>
2024-11-27 22:39:39 +0530

    Bubble, Insetion, Selection, Counting Sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+class Sorting{
+    public static void SelectionSort(int arr[]){
+        for(int turn=0; turn<arr.length-2; turn++){
+            int minPos = turn;
+            for(int j=turn+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[turn];
+            arr[turn] = temp;
+        }
+        
+    }
+
+    public static void printArr(int arr[]){
+        for(int i =0;  i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {5, 4, 1, 3, 2};
+        SelectionSort(arr);
+        printArr(arr);
+    }
+}
