e54bb46ee6c8bbcaf12b16dee3d4f0567943d98b
GAGANDASH002 <gagandash002@gmail.com>
2024-11-27 22:38:19 +0530

    Tree Traversal

---
+++ trees/Inorder.java
@@ -0,0 +1,54 @@
+class Main {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            // Boundary check for index
+            if (index >= nodes.length || nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes); // Recursive call for the left child
+            newNode.right = buildTree(nodes); // Recursive call for the right child
+
+            return newNode;
+        }
+    
+
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    }
+    
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        if (root != null) {
+            System.out.println("Root of the tree: " + root.data);
+        } else {
+            System.out.println("Tree is empty");
+        }
+
+        tree.inorder(root);
+    }
+}
---
+++ trees/Preorder.java
@@ -0,0 +1,54 @@
+class Main {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            // Boundary check for index
+            if (index >= nodes.length || nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes); // Recursive call for the left child
+            newNode.right = buildTree(nodes); // Recursive call for the right child
+
+            return newNode;
+        }
+    
+
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    }
+    
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        if (root != null) {
+            System.out.println("Root of the tree: " + root.data);
+        } else {
+            System.out.println("Tree is empty");
+        }
+
+        tree.preorder(root);
+    }
+}
